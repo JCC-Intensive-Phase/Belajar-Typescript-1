68b97c6fee02a06e3bd304fb51978ecee73d5104
tiara <niceefforts42@gmail.com>
2021-12-28 07:18:07 +0700

    latihan typescript

---
+++ latihan-typescript/function.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const article_response_1 = require("./article.response");
+const showArticle = (article) => {
+    return article;
+};
+console.log(showArticle(article_response_1.data.data));
