ce4c64fef7229ebc8036b46f6a06d6ecec4c8099
Agung Hidayat <agunghidayat10@gmail.com>
2021-12-28 10:36:46 +0700

    Day 1 : Article Response

--- Latihan Typescript (Agung Hidayat)/Day 1 Article Response/functions/getPublishedArticle.js
+++ Latihan Typescript (Agung Hidayat)/Day 1 Article Response/functions/getPublishedArticle.js
@@ -2,8 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function getAuthor(articles) {
     let data = articles.filter((article) => {
-        var _a;
-        return ((_a = article.attributes) === null || _a === void 0 ? void 0 : _a.published) === true;
+        return article.attributes.published === true;
     });
     return data;
 }
--- Latihan Typescript (Agung Hidayat)/Day 1 Article Response/functions/getPublishedArticle.ts
+++ Latihan Typescript (Agung Hidayat)/Day 1 Article Response/functions/getPublishedArticle.ts
@@ -2,7 +2,7 @@ import { Attributes } from '../types/types'
 
 function getAuthor<Type extends { attributes : Attributes }>(articles: Type[]): Type[] | undefined {
     let data = articles.filter((article) => {
-        return article.attributes?.published === true
+        return article.attributes.published === true
     })
     return data
 }
